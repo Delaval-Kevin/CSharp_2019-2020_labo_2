f4b51edcf62a7c90caa011ad8733debde5b5655c
Delaval <delaval.k60@gmail.com>
2020-04-27 11:10:17 +0200

    :checkered_flag: Finish

---
+++ Dossier_2/ClubUI/DateToDate.cs
@@ -0,0 +1,28 @@
+﻿/***********************************************************/
+/*Auteur : DELAVAL Kevin                                   */
+/*Groupe : 2203                                            */
+/*Application : Gestion d'un club de motocross             */
+/*Cours : C# - partie 3 du labo                            */
+/*Date de la dernière mise à jour : 24/04/2020             */
+/***********************************************************/
+
+using System;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace ClubUI
+{
+    class DateToDate : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            DateTime dateTime = (DateTime)value;
+            return dateTime.ToString("dd/MM/yyyy");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
